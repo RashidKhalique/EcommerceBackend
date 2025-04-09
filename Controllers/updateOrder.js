cf02bfb89c74ad33b4ab15ec747e5ea043516cf4
Rashid Khalique <91251671+RashidKhalique@users.noreply.github.com>
2025-04-09 22:44:00 +0500

    erupdateOrder.js

--- Controllers/updateOrder.js
+++ Controllers/updateOrder.js
@@ -4,18 +4,19 @@ import OrderManagement from "../model/OrderManagementdb.model.js";
 const updateOrder = async (req, res) => {
     try {
       const { orderId, DeliveryStatus, DeliveredDate } = req.body;
-        console.log(req.body)
+  
   
       // Use findOne instead of findById
       const order = await OrderManagement.findOne({ orderId: orderId });
       if (!order) {
         return res.status(404).json({ success: false, message: "Order not found." });
       }
-  
+        console.log(order)
       order.DeliveryStatus = DeliveryStatus;
       order.DeliveredDate = DeliveredDate;
   
       const updatedOrder = await order.save(); // Save the updated order
+              console.log("update : ",updatedOrder)
       return res.status(200).json({ success: true, message: "Order updated successfully", order: updatedOrder });
     } catch (err) {
       console.error(err);
